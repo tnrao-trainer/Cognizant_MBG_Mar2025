7fa950d0359c794a27a4b8617170076c37b50879
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-03-28 15:02:02 +0530

    Add files via upload

---
+++ 03.Module3_C#_Programming/28-03-2025/Examples/06.Program_OOPs_Overriding_ToString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Employee
+    {
+        // Properties
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {           
+            return $"{EmpId}, {Name}, {Salary}"; ;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Employee empObj = new Employee();
+            empObj.EmpId = 10256;
+            empObj.Name = "Scott";
+            empObj.Salary = 45000; 
+
+            Console.WriteLine(empObj); //  invokes ToString() internally
+            Console.WriteLine(empObj.ToString());
+
+            Console.ReadLine();
+        }
+    } 
+}
