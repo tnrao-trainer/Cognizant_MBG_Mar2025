ddc32cdc4416e32dcf7fd77412d90deac4a5b93b
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-04-18 18:17:06 +0530

    Add files via upload

---
+++ 04.Module4_ASP.NET_Agile_DevOps/Day26_18-04-2025/ASP.NET_MVC_Project/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class StudentController : Controller
+    {
+        private SchoolDbContext _context;
+         
+        public StudentController(SchoolDbContext context) {
+        
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult  Index()
+        {
+            List<Student> stList = _context.Students.ToList();
+            return View( stList  );
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(obj); 
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ 04.Module4_ASP.NET_Agile_DevOps/Day26_18-04-2025/ASP.NET_MVC_Project/Models/EntityModelClasses.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication5.Models
+{
+    // Entity Class :  It represent database table
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string Email { get; set; }
+    }
+
+
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options)
+         : base(options)
+        {
+
+        }
+
+    }
+}
