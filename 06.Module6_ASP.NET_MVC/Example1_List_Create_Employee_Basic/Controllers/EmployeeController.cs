1b2c84b882c2762fdde579d29ac53116d30eabd1
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-04-30 12:39:02 +0530

    Add files via upload

---
+++ 06.Module6_ASP.NET_MVC/Example1_List_Create_Employee_Basic/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication37.Models;
+
+namespace WebApplication37.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public static List<Employee> empList = new List<Employee>()
+            {
+                new Employee { Empno = 1001, Ename = "John Smith", Job = "Developer", Sal = 75000, Deptno = 10 },
+                new Employee { Empno = 1002, Ename = "Mary Johnson", Job = "Manager", Sal = 85000, Deptno = 20 },
+                new Employee { Empno = 1003, Ename = "James Brown", Job = "Analyst", Sal = 65000, Deptno = 10 },
+                new Employee { Empno = 1004, Ename = "Lisa Davis", Job = "Designer", Sal = 70000, Deptno = 30 }
+        };
+
+        public IActionResult Index()
+        {            
+
+            return View(empList);
+        }
+
+        [HttpGet]
+        public IActionResult  Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            empList.Add(obj);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 06.Module6_ASP.NET_MVC/Example1_List_Create_Employee_Basic/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication37.Models
+{
+    public class Employee
+    {
+        public int Empno { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+        public decimal Sal { get; set; }
+        public int Deptno { get; set; }
+    }
+}
