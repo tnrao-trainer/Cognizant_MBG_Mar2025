b5912e178f960f3ca4770b6e97aac4ac920d6d4e
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-04-30 15:04:50 +0530

    Add files via upload

---
+++ 06.Module6_ASP.NET_MVC/Example2_Employees_EntityFramework/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication37.Models;
+
+namespace WebApplication37.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context) {
+            _context = context; 
+        }
+      
+
+        public IActionResult Index()
+        {
+            List<Employee>  empList  =  _context.Employees.ToList();
+            return View(empList);
+        }
+
+        [HttpGet]
+        public IActionResult  Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+           
+        }
+    }
+}
